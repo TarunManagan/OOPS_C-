0a2cc6c7e13b85befa5a46104a5382dd9d64a266
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-10-03 22:05:54 +0530

    Create Volume_Cube.cpp

---
+++ Volume_Cube.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class cube{
+        public: //Allows the access of the elements even outside the class
+        double ht,breadth,len;
+        double vol_cube(){
+            cout<<"Enter the height, breadth and lenght of the cube: ";
+            cin>>ht>>breadth>>len;
+            double volume1 = ht*breadth*len;
+            return volume1;
+        }
+        void print(int vol){
+             cout<<endl<<"The volume of the cube is: "<<vol;
+        }
+};
+int main(){
+    double x;
+    cube volume; //Instantiation of object volume
+    x=volume.vol_cube();
+    volume.print(x);
+    return 0;
+}
+
+/*OUTPUT
+Enter the height, breadth and lenght of the cube: 4 5 6
+The volume of the cube is: 120
+*/
